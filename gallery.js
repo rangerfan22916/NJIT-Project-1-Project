6bb641762b9a021df3265fe1a581d1546d8d1104
rangerfan22916 <526DLichtenthal@frhsd.com>
2025-11-10 11:06:25 -0500

    Display first image and metadata

--- gallery.js
+++ gallery.js
@@ -1,67 +1,61 @@
-let mCurrentIndex = 0;
-let mImages = [];
-const mUrl = 'parks.json'; // Local JSON file
-const mWaitTime = 5000; // 5 seconds
-let mTimer;
+let mCurrentIndex = 0
+let mImages = []
+const mUrl = 'images.json'
+const mWaitTime = 5000
+let timer
 
 $(document).ready(() => {
-  $('.details').hide();
+  $('.details').hide()
 
-  // Fetch JSON data
-  fetchJSON();
+  $('.moreIndicator').click(() => {
+    $('.moreIndicator').toggleClass('rot90 rot270')
+    $('.details').slideToggle(400)
+  })
 
-  // Event handlers
-  $('.moreIndicator').click(function() {
-    $(this).toggleClass('rot90 rot270');
-    $('.details').slideToggle('slow');
-  });
+  $('#nextPhoto').click(() => showNextPhoto())
+  $('#prevPhoto').click(() => showPrevPhoto())
 
-  $('#nextPhoto').click(() => showNextPhoto());
-  $('#prevPhoto').click(() => showPrevPhoto());
+  fetchJSON()
+})
 
-  startTimer();
-});
-
-// Fetch JSON and store in mImages
 function fetchJSON() {
   $.ajax({
     url: mUrl,
     dataType: 'json',
-    success: function(data) {
-      mImages = data.images;
-      swapPhoto(); // Show first image
+    success: function (data) {
+      mImages = data.images
+      if (mImages.length > 0) {
+        mCurrentIndex = 0
+        swapPhoto()
+        startTimer()
+      }
     },
-    error: function() {
-      console.error('Failed to load JSON data.');
+    error: function () {
+      console.error('Failed to load JSON data.')
     }
-  });
+  })
 }
 
-// Display photo and metadata
 function swapPhoto() {
-  if (mImages.length === 0) return;
-
-  const currentImage = mImages[mCurrentIndex];
-  $('#photo').attr('src', currentImage.imgPath);
-  $('.location').text('Location: ' + currentImage.imgLocation);
-  $('.description').text('Description: ' + currentImage.description);
-  $('.date').text('Date: ' + currentImage.established);
+  if (mImages.length === 0) return
+  const currentImage = mImages[mCurrentIndex]
+  $('#photo').attr('src', currentImage.imgPath)
+  $('.location').text(`Location: ${currentImage.imgLocation}`)
+  $('.description').text(`Description: ${currentImage.description}`)
+  $('.date').text(`Established: ${currentImage.Established}`)
 }
 
-// Show next photo
 function showNextPhoto() {
-  mCurrentIndex = (mCurrentIndex + 1) % mImages.length;
-  swapPhoto();
+  mCurrentIndex = (mCurrentIndex + 1) % mImages.length
+  swapPhoto()
 }
 
-// Show previous photo
 function showPrevPhoto() {
-  mCurrentIndex = (mCurrentIndex - 1 + mImages.length) % mImages.length;
-  swapPhoto();
+  mCurrentIndex = (mCurrentIndex - 1 + mImages.length) % mImages.length
+  swapPhoto()
 }
 
-// Automatic timer
 function startTimer() {
-  clearInterval(mTimer);
-  mTimer = setInterval(showNextPhoto, mWaitTime);
+  clearInterval(timer)
+  timer = setInterval(showNextPhoto, mWaitTime)
 }
