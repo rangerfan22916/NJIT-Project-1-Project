0f0095ecca8bc2f5a7e7955111663a3e188bd19f
rangerfan22916 <526DLichtenthal@frhsd.com>
2025-11-14 10:56:30 -0500

    made a new 7 ancient wonders!!!!

---
+++ wonders.js
@@ -0,0 +1,69 @@
+let mCurrentIndex = 0;
+let mImages = [];
+const mUrl = 'wonders.json';
+const mWaitTime = 5000;
+let timer;
+
+$(document).ready(() => {
+  $('.details').removeClass('show');
+
+  $('.moreIndicator').click(function () {
+    $(this).toggleClass('rot90 rot270');
+    $('.details').toggleClass('show');
+  });
+
+  $('#nextPhoto').click(showNextPhoto);
+  $('#prevPhoto').click(showPrevPhoto);
+
+  fetchJSON();
+});
+
+function fetchJSON() {
+  $.ajax({
+    url: mUrl,
+    dataType: 'json',
+    success: function (data) {
+      mImages = data.images;
+      if (mImages.length > 0) {
+        mCurrentIndex = 0;
+        swapPhoto();
+        startTimer();
+      }
+    },
+    error: function () {
+      console.error('Failed to load JSON data.');
+    }
+  });
+}
+
+function swapPhoto() {
+  if (mImages.length === 0) return;
+  const currentImage = mImages[mCurrentIndex];
+  $('#photo').fadeOut(300, function () {
+    $('#photo').attr('src', currentImage.imgPath).fadeIn(300);
+  });
+  $('.location').text(`Location: ${currentImage.imgLocation}`);
+  $('.description').text(`Description: ${currentImage.description}`);
+  $('.established').text(`Established: ${currentImage.established}`);
+}
+
+function showNextPhoto() {
+  mCurrentIndex = (mCurrentIndex + 1) % mImages.length;
+  swapPhoto();
+  restartTimer();
+}
+
+function showPrevPhoto() {
+  mCurrentIndex = (mCurrentIndex - 1 + mImages.length) % mImages.length;
+  swapPhoto();
+  restartTimer();
+}
+
+function startTimer() {
+  timer = setInterval(showNextPhoto, mWaitTime);
+}
+
+function restartTimer() {
+  clearInterval(timer);
+  startTimer();
+}
